217167f964c49f45c289a1e8c29d75b168dd1379
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-14 16:19:37 +0200

    completato primo esercizio

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,18 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+// ciclo for normale
+// for (let z = 0; z < names.length; z++) {
+//     const name = names[z];
+//     console.log(name);
+// }
+
+function forEachCallback (name) {
+    console.log(name);
+}
+
+const nomi = names.forEach(forEachCallback);
+
+console.log(nomi);
